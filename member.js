2c4cd13559858f5406fe048e2a66a98a42efd486
pranaliSable <46129709+pranaliSable@users.noreply.github.com>
2024-05-08 11:56:46 +0000

    Copilot scon commit

--- member.js
+++ member.js
@@ -1,7 +1,4 @@
-function skillsmember() {
-    var skill = document.getElementsByClassName("skills");
-    var i;
-    for (i = 0; i < skill.length; i++) {
-        skill[i].style.display = "block";
-    }
+function skillsMember() {
+  var member = document.getElementById('member');
+  member.innerHTML = 'Member';
 }
